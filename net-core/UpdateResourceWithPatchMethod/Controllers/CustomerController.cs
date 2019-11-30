9d1bf5a7acbca23ff024f9dc057fe099a0344ea9
xephao <trongnhannt.it@gmail.com>
2019-11-30 13:09:16 +0700

    Add git ignore

---
+++ net-core/UpdateResourceWithPatchMethod/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpdateResourceWithPatchMethod.Controllers
+{
+    [Route("/api/customers")]
+    public class CustomerController: Controller
+    {
+        private readonly ICustomerRepo _customerRepo;
+        private readonly ILogger<CustomersController> _log;
+        private readonly IMapper _mapper;
+
+        public CustomersController(ICustomerRepo customerRepo,
+            ILogger<CustomersController> log,
+            IMapper mapper)
+        {
+            this._customerRepo = customerRepo;
+            this._log = log;
+            this._mapper = mapper;
+        }
+    }
+}
